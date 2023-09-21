ac78854b1a5bb690074909551f4582d84fe5fcde
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-21 16:10:25 +0200

    sortie du pagination container du map de slidecard
    Pour regler probleme de keys :  ca evite d avoir les bullet points trois fois , un creer a chaque iteration de la premiere map qui anglobe l autre

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -22,36 +22,37 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? 'display' : 'hide'
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? 'display' : 'hide'
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      {/* sortie du pagination container du map de slidecard */}
+
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={event.date}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx} // change idx pour index
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
