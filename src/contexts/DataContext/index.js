3bc28b6640068d0e45a655380dbe261d6c99f891
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-26 13:15:04 +0200

    Réparé last

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,17 +1,17 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import {
   createContext,
   useCallback,
   useContext,
   useEffect,
   useState,
-} from "react";
+} from 'react';
 
 const DataContext = createContext({});
 
 export const api = {
   loadData: async () => {
-    const json = await fetch("/events.json");
+    const json = await fetch('/events.json');
     return json.json();
   },
 };
@@ -19,6 +19,7 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  const [last, setLast] = useState(null);
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -30,13 +31,22 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
+  useEffect(() => {
+    const byDateDesc = data?.events.sort((evtA, evtB) =>
+      new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    );
+    if (byDateDesc) {
+      setLast(byDateDesc[0]);
+    }
+  }, [data]);
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        last,
       }}
     >
       {children}
@@ -46,8 +56,9 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
+console.log(`valeur de useData ${useData}`);
 
 export default DataContext;
