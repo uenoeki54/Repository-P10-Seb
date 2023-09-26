ec68cf64aac2f91b1f76f434a89d9af738ef9a44
uenoeki54 <uenoeki54ban@gmail.com>
2023-09-26 09:17:41 +0200

    Réparé l erreur sur imagesrc et title undefined dans eventcard

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
-import { getMonth } from "../../helpers/Date";
+import PropTypes from 'prop-types';
+import { getMonth } from '../../helpers/Date';
 
-import "./style.scss";
+import './style.scss';
 
 const EventCard = ({
   imageSrc,
@@ -12,34 +12,36 @@ const EventCard = ({
   small = false,
   ...props
 }) => (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? ' EventCard--small' : ''}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
     </div>
-  );
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
   date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   small: PropTypes.bool,
   label: PropTypes.string.isRequired,
 };
 
 EventCard.defaultProps = {
-  imageAlt: "image",
+  imageAlt: 'image',
   small: false,
-}
+  imageSrc: '',
+  title: '',
+};
 
 export default EventCard;
